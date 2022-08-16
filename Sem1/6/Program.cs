050995715794707da0bcbc8df183f9820b35e521
Dmitrii <lvov.dima1993@gmail.com>
2022-08-16 22:05:42 +0500

    выполнена задача 6

---
+++ Sem1/6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Для проверки на четность введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0) Console.WriteLine("Число четное");
+else Console.WriteLine("Число нечетное"); 
