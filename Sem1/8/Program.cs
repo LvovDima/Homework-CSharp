113d01c45d033e32dbda1199c69f7be47770b3b5
Dmitrii <lvov.dima1993@gmail.com>
2022-08-16 23:19:14 +0500

    выполнена задача 8

---
+++ Sem1/8/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+if (a<=0)
+{
+    Console.Write("Введите положительное число или программа завершится: ");
+    a = Convert.ToInt32(Console.ReadLine());
+    if (a == 1) Console.Write("В промежутке от 1 до вашего числа четных чисел нет");
+    else while (i <= a)
+        {
+            Console.Write(i+" ");
+            i+=2 ;
+        }
+}
+else
+{
+    if (a == 1) Console.Write("В промежутке от 1 до вашего числа четных чисел нет");
+    else while (i <= a)
+        {
+            Console.Write(i+" ");
+            i+=2 ;
+        }
+    
+}
