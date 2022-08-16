57812631bedf1565552c1b57f81ffbbb8c896dac
Dmitrii <lvov.dima1993@gmail.com>
2022-08-16 21:18:40 +0500

    выполнена задача 2

--- Sem1/2/Program.cs
+++ Sem1/2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if  (a > b) 
+{
+    Console.WriteLine("Максимальное из двух чисел = " + a);
+}
+else 
+{
+    Console.WriteLine("Максимальное из двух чисел = " + b);
+}
