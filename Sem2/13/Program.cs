aaac071bd152d9e87f71f8bb2d1ca6f31894492b
Dmitrii <lvov.dima1993@gmail.com>
2022-08-18 23:49:39 +0500

    выполнена задача 13

---
+++ Sem2/13/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+string input = ReadInt("Введите число: ");
+int number = Convert.ToInt32(input);
+if(number > 99)
+{
+    Console.Write("Третья цифра числа: " + Prune(input, 3) % 10);
+}
+else
+{
+    Console.Write("В введенном числе нет третьей цифры");
+}
+
+
+string ReadInt(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
+
+int Prune(string message, int i)
+{
+    int size = message.Length;
+    int res = Convert.ToInt32(message);
+    while (size > i)
+    {
+        res /= 10;
+        size--;
+    }
+    return res;
+}
+
