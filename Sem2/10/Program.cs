d34a97cb4616d549229425c46a1e297779e9f56c
Dmitrii <lvov.dima1993@gmail.com>
2022-08-18 22:34:56 +0500

    выполнена задача 10

---
+++ Sem2/10/Program.cs
@@ -0,0 +1,36 @@
+﻿int number = ReadInt("Введите трехзначное число: ");
+bool i = false;
+while (i == false)
+{
+    if (isIt3Digit(number) == i)
+    {
+        number = ReadInt("Введенное число не является трехзначным. Введите трехзначное число: ");
+    }
+    else
+    {
+        Console.WriteLine("Вторая цифра числа: " + Get2ndDigit(number));
+        i = true;
+    }
+} 
+
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int Get2ndDigit(int a)
+{
+    int res = (a / 10) - ((a / 100) * 10);
+    return res;
+}
+
+
+bool isIt3Digit (int a)
+{
+    bool i = (a > 99) && (a < 1000);
+    return i; 
+}
