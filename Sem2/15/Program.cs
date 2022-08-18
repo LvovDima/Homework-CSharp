00fa7c59202cd853634465568bac305098454045
Dmitrii <lvov.dima1993@gmail.com>
2022-08-19 00:24:34 +0500

    выполнена задача 15

---
+++ Sem2/15/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+int day = ReadInt("Введите число от 1 до 7, обозначающее день недели: ");
+
+while ((day > 7) || (day <1))
+{
+    day = ReadInt("Некорректный ввод, введите ещё раз: ");
+}
+
+if (DayOff(day) == true)
+{
+    Console.Write("Этот день является выходным");
+}
+else
+{
+    Console.Write("Этот день не является выходным");
+}
+
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool DayOff (int i)
+{
+    bool res = (i == 6) || (i == 7);
+    return res;
+}
