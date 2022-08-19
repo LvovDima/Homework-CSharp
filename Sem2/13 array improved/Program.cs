10f53fe280fae4dfaac02b9948dcbb622bff0ddb
Dmitrii <lvov.dima1993@gmail.com>
2022-08-19 22:01:42 +0500

    выполнена задача 13 array improved

--- Sem2/13 array improved/Program.cs
+++ Sem2/13 array improved/Program.cs
@@ -1,5 +1,19 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 Console.Clear();
+
+string input = ReadStr("Введите число больше 99: ");
+int[] array = Decompose(input);
+if (isItBigEnough(input) == true)
+{
+    Console.Write("Третья цифра: " + array[2]);
+}
+else
+{
+    Console.Write("Третьей цифры нет!");
+}
+
+
+
 string ReadStr(string message)
 {
     Console.Write(message);
@@ -11,22 +25,6 @@ bool isItBigEnough(string a)
     return a.Length > 2;
 }
 
-int[] Reverse(int[] a)
-{
-    int size = a.Length;
-    int i = 0;
-    int tmp = 0;
-    while (i < size / 2)
-    {
-        tmp = a[i];
-        a[i] = a[size - 1 - i];
-        a[size - i - 1] = tmp;
-        i++;
-    }
-    return a;
-}
-
-
 int[] Decompose(string word)
 {
     int size = word.Length;
@@ -35,21 +33,9 @@ int[] Decompose(string word)
     int i = 0;
     while (i < size)
     {
-        a[i] = wordToNumber % 10;
+        a[size-1-i] = wordToNumber % 10;
         wordToNumber /= 10;
         i++;
     }
-    return Reverse(a);
-}
-
-
-string input = ReadStr("Введите число больше 99: ");
-int[] array = Decompose(input);
-if (isItBigEnough(input) == true)
-{
-    Console.Write("Третья цифра: " + array[2]);
-}
-else
-{
-    Console.Write("Третьей цифры нет!");
+    return a;
 }
