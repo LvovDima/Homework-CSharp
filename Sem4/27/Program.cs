359ff9d224250b4d6680a56d7681bc00134a735c
Dmitrii <lvov.dima1993@gmail.com>
2022-08-29 00:24:50 +0500

    решена 27 задача

---
+++ Sem4/27/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int numberInt = ReadInt("Введите число: ");
+string numberStr = Convert.ToString(numberInt);
+
+SumMath(numberInt, "Математическим методом");
+SumString(numberStr, "Строчным методом");
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void SumMath(int a, string msg)
+{
+    int sum = 0;
+    while (a > 0)
+    {
+        sum += a % 10;
+        a /= 10;
+    }
+    Console.WriteLine($"Cумма цифр равна {sum}. ({msg})");
+}
+
+void SumString(string input, string msg)
+{
+    int res = 0;
+    string[] a = new string[input.Length];
+    for (int i = 0; i < input.Length; i++)
+    {
+        a[i] = input[i].ToString();
+        res += Convert.ToInt32(a[i]); 
+    }
+    Console.WriteLine($"Cумма цифр равна {res}. ({msg})");
+}
