6863ca8e67aba1651b42bc5966f57e7e16182122
Dmitrii <lvov.dima1993@gmail.com>
2022-08-29 00:33:17 +0500

    решена 29 задача

---
+++ Sem4/29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+// 5 -> [1, 2, 5, 7, 19]
+// 3 -> [6, 1, 33]
+Console.Clear();
+CreateArray(ReadInt("Введите размер массива: "));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] CreateArray (int size)
+{
+    int[] a = new int[size];
+    for (int i=0; i<a.Length; i++) 
+    {
+        a[i] = new Random().Next(0, 100);
+        Console.Write(a[i]+" ");
+    }
+    return a;
+}
