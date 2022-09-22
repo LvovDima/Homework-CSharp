09693e0cb0aa45602b5dbcbc2df226595db7b1d5
Dmitrii <lvov.dima1993@gmail.com>
2022-09-22 22:26:46 +0500

    решена 66 задача

---
+++ Sem9/66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int number1 = ReadInt("Введите 1-е число: ");
+int number2 = ReadInt("Введите 2-e число: ");
+Console.Write(RangeSum(number1, number2));
+
+
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int RangeSum (int n, int m)
+{
+    
+    if (m == n)
+        return m;
+    else 
+    {
+        int sum = m;
+        sum+=RangeSum(n, m -1);
+        return sum;
+    }
+}
